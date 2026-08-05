ed438f60323802bb218f17464b5d08a0611174ec
Vladimir PEROVIC <vladimir.perovic@gmail.com>
2026-08-05 07:12:19 +0200

    Wire local link thumbnail gateway

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { config } from "./config.js";
 import { createServer } from "./server.js";
 import { installFileLibrary } from "./file-library.js";
 import { installGalleryDownloads } from "./gallery-downloads.js";
+import { installLinkThumbnails } from "./link-thumbnails.js";
 import { installSecurityGateway } from "./security-gateway.js";
 import { closeStore } from "./store/todos.js";
 import { closeNotes } from "./store/notes.js";
@@ -20,7 +21,9 @@ function main() {
   initCrypto({ masterPassword: config.UI_PASSWORD, legacyPassword: config.ENCRYPTION_KEY });
   const server = installSecurityGateway(
     installGalleryDownloads(
-      installFileLibrary(createServer()),
+      installFileLibrary(
+        installLinkThumbnails(createServer()),
+      ),
     ),
   );
 
